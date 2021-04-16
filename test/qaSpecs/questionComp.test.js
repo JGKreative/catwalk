71a0edfb000eabcdbc1d7895486ffd077e393e7b
Jared Rogers <rogersjaredt@gmail.com>
2021-04-16 16:13:56 -0700

    created test folder for qna

---
+++ test/qaSpecs/questionComp.test.js
@@ -0,0 +1,8 @@
+const path = require('path');
+let pathToComps = path.resolve('qaComponents/question');
+import { Question } from pathToComps;
+import { render, fireEvent, screen } from '@testing-library/react'
+
+test("questionBody", () => {
+
+});
