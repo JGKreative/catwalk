a9c7cb4c344e64fa9b182170a76c3ecc72abd074
Jared Rogers <rogersjaredt@gmail.com>
2021-04-20 10:52:22 -0700

    created answer and answersList components

---
+++ client/src/qaComponents/Answer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Answer = ({ answer }) => (
+  <div>
+    <span>{answer.bodyText}</span>
+    <span>
+      {answer.username}
+      |
+      {answer.timestamp}
+    </span>
+  </div>
+);
+
+Answer.propTypes = {
+  answer: PropTypes.isRequired,
+};
+
+export default Answer;
---
+++ client/src/qaComponents/AnswersList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Answer from './Answer';
+
+const testData = [
+  {
+    bodyText: 'just google it',
+    username: 'CPT Obvious',
+    timestamp: '19-Apr-2021',
+  },
+  {
+    bodyText: 'really that is the best answer. Google is your friend',
+    username: '2LT Obvious',
+    timestamp: '20-Apr-2021',
+  },
+];
+
+const AnswersList = () => (
+  <div>
+    A:
+    <Answer answer={testData[0]} />
+    <Answer answer={testData[1]} />
+  </div>
+);
+
+// AnswersList.propTypes = {
+// };
+
+export default AnswersList;
