633fbbcccb79d659612f39b7b6ffd63c4375941a
Gabe Acevedo <gea2111@columbia.edu>
2021-04-20 15:31:26 -0700

    modularized app.jsx, moved to src

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProductDetailWrapper from './productOverviewComponents/productDetailWrapper';
+
+const App = () => (
+  <div>
+    Booya. Im the App.
+    <ProductDetailWrapper />
+  </div>
+);
+
+export default App;
