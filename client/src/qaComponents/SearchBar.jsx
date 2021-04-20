5fd112aafe2b499ee8a07f0bd07556fc1f020375
Jared Rogers <rogersjaredt@gmail.com>
2021-04-19 21:07:08 -0700

    created Search Bar skeleton

---
+++ client/src/qaComponents/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const SearchBar = () => (
+  <div>If you got more work done you could use me to search for stuff</div>
+);
+
+// Question.propTypes = {
+// };
+
+export default SearchBar;
