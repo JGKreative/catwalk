f6975064a99ec47e3eac173413492c2197218649
Kyle Harrington <harrington.kyle@gmail.com>
2021-04-19 20:44:37 -0700

    change index to jsx. Add some filler lines to simulate the ProductOverview Module

---
+++ client/src/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Cats Walking</h1>
+        <div>Product</div>
+        <div>Overview</div>
+        <div>Module</div>
+        <div>Goes</div>
+        <div>Up</div>
+        <div>HERE!!!</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
