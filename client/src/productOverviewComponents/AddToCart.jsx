e905af4e3a0f9e44b8dc0d238e852495a7f605cd
Gabe Acevedo <gea2111@columbia.edu>
2021-04-21 15:11:53 -0700

    resolving merge conflict

---
+++ client/src/productOverviewComponents/AddToCart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import ProductDetailWrapper from './productDetailWrapper.jsx';
+
+const AddToCart = () => (
+  <div>Add To Cart module goes here</div>
+);
+
+export default AddToCart;
---
+++ client/src/productOverviewComponents/ProductOverviewWrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImageGallery from './ImageGallery';
+import ProductDetailWrapper from './ProductDetailWrapper';
+
+const ProductOverviewWrapper = () => (
+  <div>
+    <ImageGallery />
+    <ProductDetailWrapper />
+  </div>
+);
+
+export default ProductOverviewWrapper;
