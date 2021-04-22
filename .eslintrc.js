4dc6c8e10324f213884e5f213dc15fcc68df5797
Jared Rogers <rogersjaredt@gmail.com>
2021-04-22 12:14:09 -0700

    allow non-camel case while destructuring API data

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,6 @@ module.exports = {
   rules: {
     'react/prop-types': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    camelcase: [2, { ignoreDestructuring: true }],
   },
 };
