7d030bb6aeccbf9b51cd29ee2ca6b0fdb6bffdc4
Jared Rogers <rogersjaredt@gmail.com>
2021-04-22 12:39:54 -0700

    QnA Parent stores data with state hook; passes data through props

--- client/src/qaComponents/QnAParentComp.jsx
+++ client/src/qaComponents/QnAParentComp.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionsList from './QuestionsList';
 import SearchBar from './SearchBar';
+import testData from '../../../test/QASpecs/testData';
 
-const QnAParentComp = () => (
-  <div>
-    Questions:
-    <SearchBar />
-    <QuestionsList />
-    <button type="button">Ask a question</button>
-    <button type="button">Show more questions</button>
-  </div>
-);
+const QnAParentComp = () => {
+  const [questionsData, setQuestionsData] = useState(testData);
+
+  return (
+    <qaDataContext.Provider>
+      Questions:
+      <SearchBar />
+      <QuestionsList questions={questionsData} />
+      <button type="button">Ask a question</button>
+      <button type="button">Show more questions</button>
+    </qaDataContext.Provider>
+  );
+};
 
 export default QnAParentComp;
+
