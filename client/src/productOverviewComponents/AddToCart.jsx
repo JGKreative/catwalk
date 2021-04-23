83611ff3f0a631b268a80efac17f0512b51f8063
Gabe Acevedo <gea2111@columbia.edu>
2021-04-23 13:17:33 -0700

    deleted unused components folder

--- client/src/productOverviewComponents/AddToCart.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import ProductDetailWrapper from './productDetailWrapper.jsx';
-
-const AddToCart = () => (
-  <div>Add To Cart module goes here</div>
-);
-
-export default AddToCart;
