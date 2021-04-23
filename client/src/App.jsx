3d10f5ba5df19931a1e556ed68651b68309dc22e
Kyle Harrington <harrington.kyle@gmail.com>
2021-04-23 11:32:04 -0700

    Update App.jsx
    removed placeholder divs that were trash

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,6 @@ import RatingsAndReviewsService from './RatingAndReviews/Wrapper';
 
 const App = () => (
   <div>
-    <h1>Cats Walking</h1>
-    <div>Product</div>
-    <div>Overview</div>
-    <div>Module</div>
-    <div>Goes</div>
-    <div>Up</div>
-    <div>HERE!!!</div>
     <RatingsAndReviewsService />
     {/* <QnA /> */}
   </div>
