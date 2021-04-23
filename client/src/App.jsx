a9a831a4ca2537100f36ba32fdb77705f202d41e
Gabe Acevedo <gea2111@columbia.edu>
2021-04-23 10:53:15 -0700

    added overview module to app render

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import OverviewContainer from './overviewComponents/OverviewContainer';
 import QnA from './qaComponents/QnAParentComp';
 
 const App = () => (
   <div>
     Booya. Im the App.
+    <OverviewContainer />
     <QnA />
   </div>
 );
