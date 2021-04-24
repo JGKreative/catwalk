47ae0336101cb0223a5c2e93aebc3c966cf07081
Gabe Acevedo <gea2111@columbia.edu>
2021-04-24 14:27:13 -0700

    added test for image gallery module

---
+++ test/OverviewSpecs/OverviewComponents.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { product, productStyles } from '/Users/gea/Desktop/HR/hr-sea16/catwalk/client/src/overviewComponents/SampleData.js';
+import ImageGallery from '/Users/gea/Desktop/HR/hr-sea16/catwalk/client/src/overviewComponents/ImageGallery.jsx';
+
+
+describe ('image gallery default view', () => {
+  beforeAll(() => {
+    render(<ImageGallery />);
+  });
+  test('default image should be first image in set', () => {
+    expect(document.getElementById('product-images-main').src).toBe(productStyles.results[0].photos[0].url);
+  });
+});
