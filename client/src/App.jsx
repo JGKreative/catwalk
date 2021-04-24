9ff464a7cff55ee299dad2a428b040d4be7835dd
rogersjared <72666689+rogersjared@users.noreply.github.com>
2021-04-24 15:15:44 -0700

    removed "head" and other merge references

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
+
 import OverviewContainer from './overviewComponents/OverviewContainer';
 import QnA from './qaComponents/QnAParentComp';
-=======
-// import QnA from './qaComponents/QnAParentComp';
->>>>>>> 87a9e9346097460f2771b0b374746dfb6a12aedd
 import RatingsAndReviewsService from './RatingAndReviews/Wrapper';
 // import QnAFillerDataUntilAPISetup from '../../test/QASpecs/testData';
 
@@ -12,7 +9,7 @@ const App = () => (
   <div>
     <OverviewContainer />
     <RatingsAndReviewsService />
-    {/* <QnA questionsData={QnAFillerDataUntilAPISetup} /> */}
+    <QnA questionsData={QnAFillerDataUntilAPISetup} />
   </div>
 );
 
