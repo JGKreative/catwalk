9eb7ae42a5b8c17889b0529a5f7ee8e3c56b3ac3
Gabe Acevedo <gea2111@columbia.edu>
2021-04-27 13:38:41 -0700

    implemented functions to fetch product styles and related products

--- client/src/overviewComponents/ApiController.js
+++ client/src/overviewComponents/ApiController.js
@@ -27,11 +27,32 @@ const api = {
         console.log(error);
       });
   },
-}
-// const fetchProductById = () => {
-// };
-// const fetchProductStyles
-// const fetchRelatedProducts
 
+  fetchProductStyles(productId) {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/${productId}/styles`, {
+      headers: authToken
+    })
+      .then((response) => {
+        console.log(response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  fetchRelatedProducts(productId) {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/${productId}/related`, {
+      headers: authToken
+    })
+      .then((response) => {
+        console.log(response.data);
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+}
 
 export default api;
