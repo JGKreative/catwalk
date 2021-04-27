e9485f03dda1946980b0206bf1b8bdce0618472d
Kyle Harrington <harrington.kyle@gmail.com>
2021-04-27 12:15:08 -0700

    add selector and state to SortOptions component

--- client/src/RatingAndReviews/SortOptions.jsx
+++ client/src/RatingAndReviews/SortOptions.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 
 const SortOptions = ({ data }) => {
-  const [sortedBy, setSortedBy] = useState('Relevance');
+  const [sortedBy, setSortedBy] = useState('relevance');
+  console.log('sortedBy: ', sortedBy)
+
   return (
     <div>
       <span style={{ fontWeight: 'bold' }}>
-        {data.length} reviews, sorted by
+        {data.length} reviews, sorted by {" "}
         <span style={{ textDecorationLine: 'underline' }}>
-          {sortedBy}
+          <select value={sortedBy} onChange={(e) => setSortedBy(e.target.value)}>
+            <option value="relevance"> Relevance </option>
+            <option value="helpfulness"> Helpfulness </option>
+            <option value="newest"> Newest </option>
+          </select>
         </span>
       </span>
     </div>
@@ -15,3 +21,7 @@ const SortOptions = ({ data }) => {
 };
 
 export default SortOptions;
+
+/*
+L10 needs to be a dropdown selector
+*/
