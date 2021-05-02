39c91a479ffc373121fd8fe10b5a9c53a76b1a54
Gabe Acevedo <gea2111@columbia.edu>
2021-05-02 00:21:50 -0700

    refactored axios requests to point to router

--- client/src/overviewComponents/ApiController.js
+++ client/src/overviewComponents/ApiController.js
@@ -1,12 +1,20 @@
-const axios = require('axios');
-import authToken from './authToken';
+import axios from 'axios';
 
 const api = {
-  fetchAllProducts(callback) {
-    return axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products', {
-      headers: authToken
-    })
+  // fetchAllProducts(callback) {
+  //   return axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products')
+  //     .then((response) => {
+  //       callback(response.data);
+  //     })
+  //     .catch((error) => {
+  //       callback(error);
+  //     });
+  // },
+
+  fetchProductById(productId, callback) {
+    axios.get(`/overview/productInfo/${productId}`)
       .then((response) => {
+        console.log('product response data', response.data);
         callback(response.data);
       })
       .catch((error) => {
@@ -14,29 +22,14 @@ const api = {
       });
   },
 
-  fetchProductById(productId) {
-    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/${productId}`, {
-      headers: authToken
-    })
+  fetchProductStyles(productId, callback) {
+    axios.get(`/overview/styles/${productId}`)
       .then((response) => {
-        console.log(response.data);
-        return response.data;
+        console.log('styles response data', response.data.results);
+        callback(response.data.results);
       })
       .catch((error) => {
-        console.log(error);
-      });
-  },
-
-  fetchProductStyles(productId) {
-    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/${productId}/styles`, {
-      headers: authToken
-    })
-      .then((response) => {
-        console.log(response.data);
-        return response.data;
-      })
-      .catch((error) => {
-        console.log(error);
+        callback(error);
       });
   },
 
